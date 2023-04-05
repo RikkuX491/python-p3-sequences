18388746f17231418a8c0611f4bb44625af0671d
Ricardo Guerra <RikkuG491@gmail.com>
2023-04-05 18:09:18 -0400

    Completed lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if(length == 0):
+        print([])
+    elif(length == 1):
+        print([0])
+    elif(length == 2):
+        print([0, 1])
+    else:
+        fibonacci_list = [0, 1]
+        for num in range(length - 2):
+            fibonacci_list.append(fibonacci_list[-1] + fibonacci_list[-2])
+        print(fibonacci_list)
